8b61d78a9e74f75ecd04d670d01595b575c2571d
flystewie <77213085+flystewie@users.noreply.github.com>
2023-06-10 06:44:57 -0400

    Update Linked List

--- java/LList/LinkedList.java
+++ java/LList/LinkedList.java
@@ -1,5 +1,34 @@
 package LList;
+import java.util.ArrayList;
 
-public class LinkedList {
+public class LinkedList<T> implements List<T> {
     
+    Node<T> head;
+    Node<T> tail;
+    private int size;
+    private ArrayList<T> list; // placeholder
+
+
+
+    public LinkedList() {
+        head = null ;
+        tail = null ;
+    }
+
+
+    if (list == null) {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public Node<T> getTail() {
+        return tail;
+    }
+
+
 }
--- java/LList/Node.java
+++ java/LList/Node.java
@@ -1,7 +1,39 @@
 package LList;
 
-public class Node {
+public class Node<T> {
 
-    
-    
+    // T data ;
+    // private T next ;   
+    T data;
+    Node<T> next;
+
+    public Node( T data, T next) {
+
+    }
+
+    public Node(T data) {
+        next = null;
+    }
+
+    public T getData() { 
+        return data;
+    }
+
+    // public void setData(T data) {
+    //     this.data = data;
+    // }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    // public void setNext(T next) {
+    //     this.next = next;
+    // }
+
+    Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;    }
 }
+
+
